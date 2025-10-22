c596c33845661545f52ac43b0cb109d4b1627bf2
LOKO122214 <ivailoarabadzhiev0@gmail.com>
2025-10-22 16:37:59 +0300

    Add files via upload

---
+++ StoreHelper/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreHelper
+{
+    internal class Product
+    {
+        private string name;
+        private double price;
+        public string Name
+        {
+            get { return name; }
+            set { 
+                if (!string.IsNullOrEmpty(value))
+                name = value;
+                else
+                    Console.WriteLine("Not Value");
+            
+            }
+        }
+
+        public double Price 
+        { 
+         get { return price;}
+         set { 
+                if (value > 0)
+                price = value;
+                else
+                    Console.WriteLine("Not Price");
+            }
+        }
+
+        public Product(string name,int price) 
+        {
+        Name = name;
+        Price = price;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Продукта {name} е с цена {price} лв");
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine($"В наличност");
+        }
+    }
+}
---
+++ StoreHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using StoreHelper;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Product coffe = new Product("кафе", 10);
+        coffe.PrintInfo();
+        coffe.PrintMessage();
+
+
+        Product tea = new Product("чай", 5);
+        tea.PrintInfo(); 
+        tea.PrintMessage();
+    }
+}
