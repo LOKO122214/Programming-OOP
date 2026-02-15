f92e0193e492ae7db989b5e906d1007c1067309e
LOKO122214 <ivailoarabadzhiev0@gmail.com>
2026-02-15 13:53:13 +0200

    Add files via upload

---
+++ Thermostat/Program.cs
@@ -0,0 +1,101 @@
+﻿using Thermostat;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, SmartHomeThermostat> devices = new Dictionary<string, SmartHomeThermostat>();
+        Console.Write("How many devices you want to add:");
+        byte count = byte.Parse(Console.ReadLine());
+        for (int i = 0; i < count; i++) 
+        {
+            Console.Write("Enter room:");
+            string roomName = Console.ReadLine();
+            Console.Write("Enter current temperature:");
+            double currentTemperature = double.Parse(Console.ReadLine());
+            Console.Write("Enter target temperature:");
+            double targetTemperature = double.Parse(Console.ReadLine());
+            devices[roomName] = new SmartHomeThermostat(roomName, currentTemperature, targetTemperature);
+        }
+        Console.Clear();
+        
+        
+        SmartHomeThermostat.devicescount();
+        Metods(devices);
+
+        
+    }
+
+    private static void Metods(Dictionary<string, SmartHomeThermostat> devices)
+    {
+        Console.WriteLine("1.Room List");
+        Console.WriteLine("2.Status");
+        Console.WriteLine("3.Heating time");
+        Console.Write("Which Metod you want to use:");
+        byte choose = byte.Parse(Console.ReadLine());
+
+
+        if (choose != 1 && choose != 2&& choose !=3)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+        else
+        {
+
+            switch (choose)
+            {
+                case 1:
+                    foreach (var item in devices)
+                    {
+                        Console.WriteLine(item.Key);
+                    }
+                    break;
+                
+                
+                case 2:
+                    Console.Write("Wich room you want to see:");
+                    string room = Console.ReadLine();
+                    if (devices.ContainsKey(room))
+                    {
+                        devices[room].GetStatus();
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException();
+                    }
+
+                    break;
+
+
+                case 3:
+                    Console.Write("Which room you want to see:");
+                    string hroom = Console.ReadLine();
+                    if (devices.ContainsKey(hroom))
+                    {
+                        Console.Write("For what rate you want to see(from 0.1 to 1.0:");
+                        double rate = double.Parse(Console.ReadLine());
+                        if (rate < 0.1 || rate > 1.0)
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+                        else
+                        {
+                            devices[hroom].CalculateTimeToReachTarget(rate);
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException();
+                    }
+
+                    break;
+            }
+            Console.WriteLine("Do you want to see other information y/n:");
+            char cont = char.Parse(Console.ReadLine());
+            if (cont == 'y')
+            {
+                Metods(devices);
+            }
+        }
+    }
+    }
---
+++ Thermostat/SmartHomeThermostat.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thermostat
+{
+    internal class SmartHomeThermostat
+    {
+        private string roomName;
+        private double currentTemperature;
+        private double targetTemperature;
+        private static byte devicesCount;
+
+
+        private string RoomName
+        {
+            get 
+            { 
+                return roomName; 
+            }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    roomName = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
+        
+        
+        private double CurrentTemperature
+        {
+            get
+            {
+                return currentTemperature;
+            }
+            set
+            {
+                currentTemperature = value;
+            }
+        }
+
+        private double TargetTemperature
+        {
+            get
+            {
+                return targetTemperature;
+            }
+            set
+            {
+                if (value < 5 || value >30) 
+                { 
+                  throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    targetTemperature = value;
+                }
+            }
+        }
+
+       public static void devicescount()
+        {
+            Console.WriteLine($"The Count of devices is {devicesCount}");
+        }
+
+
+        public SmartHomeThermostat()
+        {
+            currentTemperature = 20.0;
+            targetTemperature = 20.0;
+            devicesCount++;
+            roomName = "Unknown Room";
+        }
+
+        public SmartHomeThermostat(string roomName,double currentTemperature, double targettemperature)
+        {
+            this.RoomName = roomName;
+            this.CurrentTemperature = currentTemperature;
+            this.TargetTemperature = targettemperature;
+            devicesCount++;
+        }
+
+        public void GetStatus()
+        {
+            string status = "";
+            if (CurrentTemperature < TargetTemperature)
+            {
+                status = "Heating";
+            }
+            else if (CurrentTemperature == TargetTemperature)
+            {
+                status = "Reached";
+            }
+            else 
+            {
+                status = "Cooling";
+            }
+                Console.WriteLine($"Room:{RoomName}|Current:{CurrentTemperature}|Target:{TargetTemperature}|Status:{status}");
+        }
+
+        public double CalculateTimeToReachTarget(double heatingRate)
+        {
+            double deltaT = TargetTemperature - CurrentTemperature;
+            if (deltaT > 0)
+            {
+                heatingRate = deltaT / heatingRate;
+                Console.WriteLine($"Room will heat in {heatingRate} minutes");
+            }
+            else if (deltaT == 0)
+            {
+                Console.WriteLine("Room will not heat or cool");
+            }
+            else
+            {
+                heatingRate = deltaT / heatingRate;
+                Console.WriteLine($"Room will cool in {-1*(heatingRate)} minutes");
+            }
+                return heatingRate;
+        }
+
+
+    }
+        
+}
